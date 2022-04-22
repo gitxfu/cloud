3d963cd058dba94a2c238c8420e3a2d46df86b9d
gitxfu <76028008+gitxfu@users.noreply.github.com>
2022-04-21 21:48:23 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd /mnt/modelfiles
-echo test " $(mktemp XXXXXXXX.jpeg)"
+cd /home/matlabuser/inca_azure/
+chmod +x mount_fileshare.sh
+./mount_fileshare.sh
+
+chmod +x bashMonteCarloSimulation.sh
+./bashMonteCarloSimulation.sh simple.mat
+
+chmod +x bashMultiEstimate.sh
+./bashMultiEstimate.sh simple.mat
