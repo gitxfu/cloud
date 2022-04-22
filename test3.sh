13f9a6a2b7e8798613b608844adfeda40740bc43
gitxfu <76028008+gitxfu@users.noreply.github.com>
2022-04-22 08:59:02 -0400

    Update and rename test3.sh to test4.sh

--- test3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/matlabuser/inca_azure/
-chmod +x bashMonteCarloSimulation.sh
-./bashMonteCarloSimulation.sh model.mat
-./bashMonteCarloSimulation.sh ecoli.mat
-
-chmod +x bashMultiEstimate.sh
-./bashMultiEstimate.sh simple.mat
-./bashMultiEstimate.sh ecoli.mat
---
+++ test4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/matlabuser/inca_azure/
+chmod +x bashMonteCarloSimulation.sh
+./bashMonteCarloSimulation.sh simple2.mat
